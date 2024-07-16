1b5fbadb8536131a7227dbdfb543bfa8419f41e7
Jaya0611 <161932606+Jaya0611@users.noreply.github.com>
2024-07-16 22:02:02 +0530

    Add files via upload

---
+++ assignment2-2.js
@@ -0,0 +1,16 @@
+//  After printing their value assign them as null and print it again
+var Name=null;
+var Email=null;
+var Age=null;
+var address=null;
+ var Phone_Number=null;
+ var Gender=null;
+ var married=null;
+ console.log("Basic Details-:");
+ console.log("Name: "+Name);
+ console.log("Email: "+Email);
+ console.log("Age: "+Age);
+ console.log("Address: "+address);
+ console.log("Phone_Number: "+Phone_Number);
+ console.log("Gender: "+Gender);
+ console.log("Married: "+married);
