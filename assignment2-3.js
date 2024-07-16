cdb7ca62205c81c317ffe8d7fc32b1fcb69b052e
Jaya0611 <161932606+Jaya0611@users.noreply.github.com>
2024-07-16 22:02:28 +0530

    Add files via upload

---
+++ assignment2-3.js
@@ -0,0 +1,19 @@
+// Declare an object called employeeDetails and add all the above variables to this object now as keys with value as null and print this object.
+let employeeDetails={
+Name:null,
+Email:null,
+Age:null,
+address:null,
+Phone_Number:null,
+Gender:null,
+married:null
+}
+console.log("Name: "+employeeDetails.Name);
+console.log("Email: "+employeeDetails.Email);
+console.log("Age: "+employeeDetails.Age);
+console.log("address: "+employeeDetails.address);
+console.log("Phone_Number: "+employeeDetails.Phone_Number);
+console.log("Gender: "+employeeDetails.Gender);
+console.log("married: "+employeeDetails.married);
+
+
