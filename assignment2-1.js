9b48c77c717356210af8db97ac311c8ea566ba09
Jaya0611 <161932606+Jaya0611@users.noreply.github.com>
2024-07-16 22:01:36 +0530

    Add files via upload

---
+++ assignment2-1.js
@@ -0,0 +1,16 @@
+// Declare all the variables in above form without initialising and print their value.
+var Name;
+var Email;
+var Age;
+var address;
+ var Phone_Number;
+ var Gender;
+ var married;
+ console.log("Basic Details-:");
+ console.log("Name: "+Name);
+ console.log("Email: "+Email);
+ console.log("Age: "+Age);
+ console.log("Address: "+address);
+ console.log("Phone_Number: "+Phone_Number);
+ console.log("Gender: "+Gender);
+ console.log("Married: "+married);
